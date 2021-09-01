2b5ae9684a2647deb90a644af5abf8d9c334c5af
Fergus Farrell <fergus@batch.nz>
2021-09-01 16:59:16 +1200

    Bootstrapped tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Batch Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
@@ -29,9 +33,25 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-gatsby-cloud`,
+      options: {
+        mergeSecurityHeaders: false,
+        headers: {
+          "/*": [
+            "Content-Security-Policy: frame-ancestors 'self' https://*.batch.dev/",
+            "X-Frame-Options: ALLOW-FROM https://frankie.batch.dev/",
+          ],
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-craft`,
+      options: {
+        craftGqlToken: process.env.CRAFT_GQL_TOKEN,
+        craftGqlUrl: process.env.CRAFT_GQL_URL,
+      },
+    },
   ],
 }
